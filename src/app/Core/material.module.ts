07594acd869acd791b2a55055741429b3cc7d02a
PrakashChandra <46310172+prakashnerale@users.noreply.github.com>
2019-11-19 14:41:04 +0530

    update

---
+++ src/app/Core/material.module.ts
@@ -0,0 +1,44 @@
+import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+  MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule,MatGridListModule
+} from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatListModule} from '@angular/material/list';
+@NgModule({
+  imports: [
+  CommonModule, 
+  MatToolbarModule,
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatDialogModule,
+  MatTableModule,
+  MatMenuModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatGridListModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatListModule
+  ],
+  exports: [
+  CommonModule,
+   MatToolbarModule, 
+   MatButtonModule, 
+   MatCardModule, 
+   MatInputModule, 
+   MatDialogModule, 
+   MatTableModule, 
+   MatMenuModule,
+   MatIconModule,
+   MatProgressSpinnerModule,
+   MatGridListModule,
+   MatSelectModule,
+   MatSlideToggleModule,
+   MatListModule
+   ],
+})
+export class CustomMaterialModule { }
