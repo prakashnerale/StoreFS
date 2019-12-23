026f260c0667b6e8d6c60d42767a9b648ac2e763
PrakashChandra <46310172+prakashnerale@users.noreply.github.com>
2019-12-23 10:43:25 +0530

    update

--- src/app/Core/app.routing.ts
+++ src/app/Core/app.routing.ts
@@ -1,8 +1,24 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { MyAccountComponent } from '../components/my-account/my-account.component';
+
 
 const appRoutes: Routes = [
-	
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'myAccount',
+    component: MyAccountComponent
+  }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { MyAccountComponent } from './components/my-account/my-account.component';
 
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component:HomeComponent
-},
-{
-  path: 'myAccount',
-  component:MyAccountComponent
-}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
